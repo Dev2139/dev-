0d41ba40f33aa49198d626db15d9522ed2200467
Dev2139 <dev.patel.codinggita@gmail.com>
2025-02-15 12:09:26 +0530

    aaaaa

--- api/app.js
+++ api/app.js
@@ -163,4 +163,9 @@ function moveToNextPlayer() {
     }
 }
 
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
 module.exports = app;
